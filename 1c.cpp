e1b3f04e7d93780a9c9c65b9b3790d50ba9b1073
p0ice <67363618+zxhiend@users.noreply.github.com>
2024-09-05 21:03:33 +0700

    Updated code

--- 1c.cpp
+++ 1c.cpp
@@ -7,23 +7,30 @@ int main(){
     string Sentinel;
 
     cout << "Apakah anda mau cetak? (YA/EXIT): ";
-
     getline(cin, Sentinel);
-
-    if(Sentinel == "exit" || Sentinel == "EXIT"){
-        cout << "Keluar dari program." << endl;
+    if (Sentinel == "exit" || Sentinel == "EXIT"){
+        cout << "Program selesai.";
         return 0;
-    } else if(Sentinel == "ya" || Sentinel == "YA"){
-        cout << "Masukkan limit output: ";
-        cin >> y;
-        
+    } 
+    cout << "Masukkan limit output: ";
+    cin >> y;
+    
+    while (Sentinel != "EXIT"){
         for(int i=1; i <= y; i++){
             cout << i << ". Program C++" << endl;
-         }
-
-     } else{
-        cout << "Input Invalid! Exitting..." << endl;
-     }
+        }
+        
+        cin.ignore(); // https://stackoverflow.com/questions/25475384/when-and-why-do-i-need-to-use-cin-ignore-in-c
+        cout << "Apakah anda mau cetak lagi? (YA/EXIT): ";
+        getline(cin, Sentinel);
+        if (Sentinel == "YA" || Sentinel == "ya"){
+            cout << "Masukkan limit output: ";
+            cin >> y;
+        }
+    }
+    cout << "Program selesai." << endl;
+    return 0;
+    
 }
 
 
